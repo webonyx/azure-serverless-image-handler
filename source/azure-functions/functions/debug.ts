362de31e363c221b8a3b3f9f8f136af983c40717
Viet Pham <viet@webonyx.com>
2023-06-05 22:04:11 +0700

    Hide more keys

--- source/azure-functions/functions/debug.ts
+++ source/azure-functions/functions/debug.ts
@@ -7,12 +7,7 @@ app.http("debug", {
     const env = process.env;
 
     for (const name in env) {
-      if (
-        name.includes("AWS_ACCESS") ||
-        name.includes("AWS_SECRET") ||
-        name.endsWith("CONNECTION_STRING") ||
-        env[name].includes("Key=")
-      ) {
+      if (name.includes("KEY") || name.endsWith("CONNECTION_STRING") || env[name].includes("Key=")) {
         env[name] = "[REDACTED]";
       }
     }
